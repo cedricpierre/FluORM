b3ea6de856f59cf3ed71410a51b7b88a68e8a9ae
Cedric Pierre <cedric.pierre@scala.com>
2025-04-23 20:55:27 +0200

    update model

--- examples/models/Media.ts
+++ examples/models/Media.ts
@@ -1,10 +1,19 @@
 import { BelongsTo, HasMany } from '../../src/decorators'
 import { User } from './User'
-import { BaseModel } from '../../src/BaseModel'
+import { Attributes, BaseModel } from '../../src/BaseModel'
 import { Relation } from '../../src/Builder'
 import { Thumbnail } from './Thumbnail'
 
-export class Media extends BaseModel<any> {
+export interface IMedia extends Attributes {
+    id: string
+    name: string
+    size: string
+    extension: string
+    mime_type: string
+    url: string
+}
+
+export class Media extends BaseModel<IMedia> {
     static resource = 'medias'
 
     @BelongsTo(() => User as any)
--- examples/models/User.ts
+++ examples/models/User.ts
@@ -4,14 +4,14 @@ import { BaseModel } from '../../src/BaseModel'
 import { Relation } from '../../src/Builder'
 import { type Attributes } from '../../src/BaseModel'
 
-interface UserAttributes extends Attributes {
+interface IUser extends Attributes {
     name: string
     email: string
     created_at?: string
     updated_at?: string
 }
 
-export class User extends BaseModel<UserAttributes> {
+export class User extends BaseModel<IUser> {
     static resource = 'users'
 
     @HasMany(() => Media as any)
