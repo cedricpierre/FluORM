e14c700d27410df8e5dcaf400e23262ce2df5ace
Cedric Pierre <cedric.pierre@scala.com>
2025-04-23 20:41:15 +0200

    fix url builder

--- src/URLQueryBuilder.ts
+++ src/URLQueryBuilder.ts
@@ -1,29 +1,28 @@
 export class URLQueryBuilder {
-    private filters: Record<string, any> = {}
-    private includes: string[] = []
-    private sort: string[] = []
+    private _filters: Record<string, any> = {}
+    private _includes: string[] = []
+    private _sort: string[] = []
     private _limit?: number
     private _offset?: number
     private _page?: number
     private _perPage?: number
 
     where(obj: Record<string, any>): this {
-        this.filters = { ...this.filters, ...obj }
-        return this
+        return this.filter(obj)
     }
 
     filter(obj: Record<string, any>): this {
-        this.filters = { ...this.filters, ...obj }
+        this._filters = { ...this._filters, ...obj }
         return this
     }
 
     include(rel: string | string[]): this {
-        this.includes.push(...(Array.isArray(rel) ? rel : [rel]))
+        this._includes.push(...(Array.isArray(rel) ? rel : [rel]))
         return this
     }
 
     orderBy(field: string, direction: string = 'asc'): this {
-        this.sort.push(`${direction === 'desc' ? '-' : ''}${field}`)
+        this._sort.push(`${direction === 'desc' ? '-' : ''}${field}`)
         return this
     }
 
@@ -49,13 +48,14 @@ export class URLQueryBuilder {
     }
 
     toObject(): Record<string, any> {
-        const obj: Record<string, any> = { ...this.filters }
-        if (this.includes.length) obj.include = this.includes.join(',')
-        if (this.sort.length) obj.sort = this.sort.join(',')
+        const obj: Record<string, any> = { ...this._filters }
+        if (this._includes.length) obj.include = this._includes.join(',')
+        if (this._sort.length) obj.sort = this._sort.join(',')
         if (this._limit !== undefined) obj.limit = this._limit
         if (this._offset !== undefined) obj.offset = this._offset
         if (this._page !== undefined) obj.page = this._page
         if (this._perPage !== undefined) obj.per_page = this._perPage
+
         return obj
     }
 }
